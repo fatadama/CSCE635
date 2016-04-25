192c8fe43dabfc90c73dce532faf603dc25a22cd
TWoodbury <woodbury.tim@gmail.com>
2016-04-25 15:59:02 -0500

    Added new python files for the XBee bridge code. Development for Windows 7 on Python 3.5 using pyserial and the websockets library at: https://pypi.python.org/pypi/websockets Started some basic class definitions

---
+++ python/xbee_bridge/xbee_bridge.py
@@ -0,0 +1,19 @@
+import sys
+# add path to serial protocol
+sys.path.append('..\ESP')
+
+# serial for talking to xbee
+import serial
+
+# dependencies for websocket package
+import asyncio
+# websocket package for IPC
+import websockets
+
+# import the bridge class definition
+
+def main():
+    return 0
+
+if __name__ == "__main__":
+    main()
---
+++ python/xbee_bridge/xbee_bridge_state.py
@@ -0,0 +1,73 @@
+# numpy for arrays and potentially for operations later
+import numpy as np
+
+## Conversion factor for (degrees->radians->arc length at mean Earth equatorial radius)
+GPS_D2R_RE = 0.0111318845
+## Smoothing factor to use on measured GPS values
+SMOOTH_ALPHA = 0.1
+
+## Lowpass filter an input to return a new, smoothed state
+#
+# Typical values of alpha are 0.05-0.15
+# @param[in] xlast the prior value
+# @param[in] xnew the new measurement
+# @param[in] alpha: the smoothing factor
+# @param[out] xout: xout = alpha*xnew + (1.0-alpha)*xlast
+def lowpass(xlast,xnew,alpha):
+    return alpha*xnew + (1.0-alpha)*xlast
+
+class gpsState():
+    def __init__(self):
+        # longitude (degrees) positive == east
+        self.lon = 0.0
+        # latitude (degrees) positive == north
+        self.lat = 0.0
+        # GPS time in seconds (from receiver)
+        self.time = 0.0
+        self.x = 0.0
+        self.y = 0.0
+        self.v = 0.0
+        self.hdg = 0.0
+        self.lon_home = 0.0
+        self.lat_home = 0.0
+        # initialization flag: set to False until we receive at least one GPS update
+        self.ready = False
+    ## Read in raw lat/lon/time from the XBee and update the inernal state
+    #
+    # Smooth all inputs with a 0.1 RC lowpass filter
+    # @param[in] lon_int ( longitude (deg) x 10^7 ) as an integer
+    # @param[in] lat_int ( Latitude (deg) x 10^7 ) as an integer
+    # @param[in] t time in seconds
+    # @param[in] vel speed in m/s
+    # @param[in] h heading in radians, relative to north. (positive == east)
+    def update(self,lon_int,lat_int,t,vel,h):
+        if self.ready:
+            self.lon = lowpass(self.lon, 1.0e-7*float(lon_int), SMOOTH_ALPHA)
+            self.lat = lowpass(self.lat, 1.0e-7*float(lat_int), SMOOTH_ALPHA)
+            self.v = lowpass(self.v,vel,SMOOTH_ALPHA)
+            self.hdg = lowpass(self.hdg,h,SMOOTH_ALPHA)
+        else:
+            self.lon = (1.0e-7*float(lon_int))
+            self.lat = (1.0e-7*float(lat_int))
+            self.v = vel
+            self.hdg = h
+        self.time = t
+        # update the X Y values
+        self.latLon2XY()
+        return
+    ## update the x y position using the current lat/lon measurement
+    #
+    # Recall: North-east-down local frame.
+    # X = (lat-lat_home)*GPS_D2R_RE
+    # Y = (lon-lon_home)*GPS_D2R_RE
+    def latLon2XY(self):
+        self.x = (self.lat-self.lat_home)*GPS_D2R_RE
+        self.y = (self.lon-self.lon_home)*GPS_D2R_RE
+        return
+
+class xbee_bridge_state():
+    def __init__(self):
+        ## gpsState object for holding the state of EMILY
+        gpsState gps_state();
+        ## control mode
+        return
